89d69755bf0f0103023d15bf5c81dbfa5b67e74c
noelgarber <104410131+noelgarber@users.noreply.github.com>
2022-08-16 03:37:46 -0400

    Major changes to Step3_Bait_Specificity_Matrix_Generator
    Corrected an error in points handling (comparator count bias correction) and added options for how to define specificity.
    
    Option 1 = comparing hits specific to Comparator 1 to hits specific to Comparator 2. Option 2 = =comparing hits specific to Comparator 1 to hits that EITHER bind Comparator 2 specifically OR that bind both Comparators 1 & 2 equally (nonspecifically).
    
    Also added a feature for importing the most recent results from Step 2.

--- Step3_Bait_Specificity_Matrix_Generator.py
+++ Step3_Bait_Specificity_Matrix_Generator.py
@@ -5,7 +5,8 @@
 import numpy as np
 import pandas as pd
 import os
-from PACM_General_Functions import FilenameSubdir, NumInput
+import pickle
+from PACM_General_Functions import FilenameSubdir, NumInput, ListInputter
 from PACM_General_Vars import list_aa, list_aa_no_phos
 
 print("----------------")
@@ -14,7 +15,12 @@ print("This script generates SLiM bait specificity scores based on inputted bait
 print("IMPORTANT: Specificity scores are, in our experience, only interpretable if the underlying SLiM score passes a defined threshold; they cannot be interpreted in isolation.")
 print("----------------")
 
-filename = input("Enter the filename containing pairwise scored densitometry data, located in /Output:  ")
+with open("Step2_Results_Filename.ob", "rb") as f:
+	filename = pickle.load(f)
+use_last_generated = input("The last generated file from Step 2 was " + filename + ". Would you like to use a different filename? (Y/N)  ")
+if use_last_generated == "Y": 
+	filename = input("Enter the filename containing pairwise scored densitometry data, located in /Output:  ")
+
 dens_final_df = pd.read_csv(FilenameSubdir("Output", filename), index_col = 0)
 
 with open("list_of_baits.ob", "rb") as lob:
@@ -26,62 +32,116 @@ with open("list_of_baits.ob", "rb") as lob:
 
 slim_length = 15 #This is the length of the motif being analyzed
 positions = np.arange(1, slim_length + 1)
-bait1_inputted = input("First bait to compare (1 of 2):  ")
-bait2_inputted = input("Second bait to compare (2 of 2):  ")
+print("This script allows pooling of similar baits; for example, specificity between Protein A and Proteins B & C.")
+
+def CheckPooling(bait_number): 
+	print("Setting bait(s) for use as Comparator #" + str(bait_number))
+	bait_pool_list = ListInputter("Input the baits one at a time.")
+	return bait_pool_list
+
+bait1_inputted = CheckPooling(1)
+bait2_inputted = CheckPooling(2)
+print("There are two options for scoring method:")
+print("    Option 1 --> Score to predict hits specific to Comparator 1 vs. hits specific to Comparator 2")
+print("    Option 2 --> Score to predict hits specific to Comparator 1 vs. all hits that bind Comparator 2, either equally or specifically")
+
+valid_input = False
+while not valid_input: 
+	points_handling = NumInput("Which option? Select 1 or 2:")
+	if points_handling == 1 or points_handling == 2: 
+		valid_input = True
 
 #--------------------------------------------------------------------------
 
-def SpecRatioBias(b1, b2, source_df, thres, passes_col = "Significant"): 
+def LeastDiffFold(b1_list, b2_list, index, lookup_df): 
+	least_diff_log2fc = 9999
+	least_diff_bait_combo = []
+	for b1 in b1_list: 
+		for b2 in b2_list: 
+			if b1 != b2: 
+				b1_b2_log2fc = lookup_df.at[index, b1 + "_" + b2 + "_log2fc"]
+				if abs(b1_b2_log2fc) < abs(least_diff_log2fc): 
+					least_diff_log2fc = b1_b2_log2fc
+					least_diff_bait_combo = [b1, b2]
+	return least_diff_log2fc, least_diff_bait_combo
+
+def SpecRatioBias(bait1_pool, bait2_pool, source_df, pos_thres, neg_thres, passes_col = "Significant"): 
 	above_thres_count = 0
 	below_neg_thres_count = 0
+
 	for i in np.arange(len(source_df)): 
 		passes = source_df.at[i, passes_col]
 
-		bait1_bait2_log2fc = source_df.at[i, bait1 + "_" + bait2 + "_log2fc"]
+		bait1_bait2_log2fc, bait1_bait2_list = LeastDiffFold(bait1_pool, bait2_pool, i, source_df)
 
-		if passes == "Yes" and bait1_bait2_log2fc >= thres: 
+		if passes == "Yes" and bait1_bait2_log2fc >= pos_thres: 
 			above_thres_count += 1
-		elif passes == "Yes" and bait1_bait2_log2fc <= (-1 * thres): 
+		elif passes == "Yes" and bait1_bait2_log2fc <= neg_thres: 
 			below_neg_thres_count += 1
 	if below_neg_thres_count != 0: 
 		ratio = above_thres_count / below_neg_thres_count
 	else: 
-		ratio = 1
-	return ratio, bait1_bait2_log2fc
+		ratio = 100 #Arbitrary value; would be infinity
+	return ratio
 
-def PointsFinder(output_df, hthres, mthres, bias_ratio, seq_length, sequence_log2fc, pass_call, aalist): 
+def PointsFinder(output_df, hthres, mthres, bias_ratio, sequence, seq_length, seq_log2fc, pass_call, aalist): 
+	#print(output_df)
 	indices = np.arange(seq_length)
 	if hthres == "Continuous" or mthres == "Continuous": 
 		for n in indices: 
 			m = n + 1
 			for aa in aalist: 
-				if aa == seq[n]: 
-					points = sequence_log2fc
+				if aa == sequence[n]: 
+					points = seq_log2fc
 					if pass_call == "Yes": 
-						generic_matrix_df.at[aa, "#" + str(m)] += points
-	else: 
+						output_df.at[aa, "#" + str(m)] += points
+	elif points_handling == 1: 
 		for n in indices: 
 			m = n + 1
 			for aa in aalist: 
-				if aa == seq[n]: 
+				if aa == sequence[n]: 
 					#Calculation of points: 
-					if sequence_log2fc >= hthres: 
+					if seq_log2fc >= hthres: 
 						points = 2
-					elif sequence_log2fc >= mthres: 
+					elif seq_log2fc >= mthres: 
 						points = 1
-					elif sequence_log2fc <= (mthres * -1): 
+					elif seq_log2fc <= (mthres * -1): 
 						points = -1 * bias_ratio
-					elif sequence_log2fc <= (hthres * -1): 
+					elif seq_log2fc <= (hthres * -1): 
 						points = -1 * bias_ratio
 					else: 
 						points = 0
 					#Pass-conditional point assignment: 
 					if pass_call == "Yes": 
-						generic_matrix_df.at[aa, "#" + str(m)] += points
+						output_df.at[aa, "#" + str(m)] += points
+					else: 
+						continue
+	elif points_handling == 2: 
+		for n in indices: 
+			m = n + 1
+			for aa in aalist: 
+				if aa == sequence[n]: 
+					#Calculation of points: 
+					if seq_log2fc >= hthres: 
+						points = 2
+					elif seq_log2fc >= mthres: 
+						points = 1
+					elif seq_log2fc <= (mthres * -1): 
+						points = -2 * bias_ratio
+					elif seq_log2fc <= 0: 
+						points = -1 * bias_ratio
+					else: 
+						points = 0
+					#Pass-conditional point assignment: 
+					if pass_call == "Yes": 
+						output_df.at[aa, "#" + str(m)] += points
 					else: 
 						continue
 
-def SpecWeightedMatrix(motif_length, bait1, bait2, source_dataframe, amino_acid_list, log2fc_high_thres, log2fc_mid_thres): 
+def SpecWeightedMatrix(motif_length, bait1_list, bait2_list, source_dataframe, amino_acid_list, log2fc_high_thres, log2fc_mid_thres): 
+	print("Executing SpecWeightedMatrix()")
+	updated_output_df = source_dataframe.copy()
+
 	#Construct empty matrix
 	list_pos = []
 	for i in range(1, motif_length + 1): 
@@ -90,23 +150,32 @@ def SpecWeightedMatrix(motif_length, bait1, bait2, source_dataframe, amino_acid_
 	generic_matrix_df = generic_matrix_df.fillna(0)
 
 	#Adjust for inequality of hits specific to one bait vs. the other
-	above_below_thres_ratio = SpecRatioBias(bait1, bait2, source_dataframe, log2fc_mid_thres) 
+	if points_handling == 1: 
+		above_below_thres_ratio = SpecRatioBias(bait1_pool = bait1_list, bait2_pool = bait2_list, source_df = source_dataframe, 
+			pos_thres = log2fc_mid_thres, neg_thres = (-1 * log2fc_mid_thres), passes_col = "Significant")
+	elif points_handling ==2: 
+		above_below_thres_ratio = SpecRatioBias(bait1_pool = bait1_list, bait2_pool = bait2_list, source_df = source_dataframe, 
+			pos_thres = log2fc_mid_thres, neg_thres = 0, passes_col = "Significant")
+	else: 
+		raise Exception("Unacceptable value for points_handling! Value:" + str(points_handling))
+	print("above_below_thres_ratio =", above_below_thres_ratio)
 
 	#Score the sequences
-	for i in np.arange(len(source_dataframe)): 
-		seq = source_dataframe.at[i, "BJO_Sequence"]
-		seq = list(seq) #converts to aa list
+	for i in np.arange(len(updated_output_df)): 
+		seq = updated_output_df.at[i, "BJO_Sequence"]
+		#seq = list(seq) #converts to aa list
 
-		passes = source_dataframe.at[i, "Significant"]
+		passes = updated_output_df.at[i, "Significant"]
 
-		bait1_bait2_log2fc = source_dataframe.at[i, bait1 + "_" + bait2 + "_log2fc"]
+		bait1_bait2_log2fc, bait1_bait2_list = LeastDiffFold(bait1_list, bait2_list, i, updated_output_df)
+		updated_output_df.at[i, "Least_Different_Log2fc"] = bait1_bait2_log2fc
 
 		PointsFinder(output_df = generic_matrix_df, hthres = log2fc_high_thres, mthres = log2fc_mid_thres, bias_ratio = above_below_thres_ratio, 
-			seq_length = slim_length, sequence_log2fc = bait1_bait2_log2fc, pass_call = passes, aalist = amino_acid_list)
+			sequence = seq, seq_length = slim_length, seq_log2fc = bait1_bait2_log2fc, pass_call = passes, aalist = amino_acid_list)
 
 	generic_matrix_df = generic_matrix_df.astype("float32")
 
-	return generic_matrix_df
+	return generic_matrix_df, updated_output_df
 
 #--------------------------------------------------------------------------
 
@@ -128,7 +197,7 @@ print("---")
 
 #Construction of weighted matrix
 
-weighted_matrix_df = SpecWeightedMatrix(slim_length, bait1_inputted, bait2_inputted, dens_final_df, list_aa, log2fc_upper_thres, log2fc_lower_thres)
+weighted_matrix_df, dens_final_scored_df = SpecWeightedMatrix(slim_length, bait1_inputted, bait2_inputted, dens_final_df, list_aa, log2fc_upper_thres, log2fc_lower_thres)
 
 merge_phospho = input("Generated matrix. Merge phospho-residues with their unphosphorylated counterparts? (Y/N)  ")
 if merge_phospho == "Y": 
@@ -151,11 +220,15 @@ print("-------------------")
 
 print("Computing edited densitometry data file containing scores for each peptide.")
 
-#Begin scoring algorithm construction
-
-dens_final_scored_df = dens_final_df.copy()
-
 #Calculate individual residue scores and sum them
+
+spec_col_name = ""
+for a in bait1_inputted: 
+	spec_col_name = spec_col_name + a + "_"
+spec_col_name = spec_col_name + "vs_"
+for b in bait2_inputted: 
+	spec_col_name = spec_col_name + b + "_"
+spec_col_name = spec_col_name + "Specificity_Score"
 
 for i in np.arange(len(dens_final_scored_df)): 
 	seq = dens_final_scored_df.at[i, "No_Phos_Sequence"]
@@ -165,7 +238,17 @@ for i in np.arange(len(dens_final_scored_df)):
 		score_current_position = weighted_matrix_df.at[res, "#" + str(j)]
 		total_score = total_score + score_current_position
 
-	dens_final_scored_df.at[i, bait1_inputted + "_" + bait2_inputted + "_Specificity_Score"] = total_score
+	dens_final_scored_df.at[i, spec_col_name] = total_score
+
+#Final adjustment and output of score transformation method
+
+#list_of_scores = dens_final_scored_df.loc[spec_col_name].tolist()
+#max_of_scores = max(list_of_scores)
+#min_of_scores = 
+
+#for i in np.arange(len(dens_final_scored_df)): 
+#	untransformed_score = dens_final_scored_df.at[i, spec_col_name]
+
 
 #Save to output
 
