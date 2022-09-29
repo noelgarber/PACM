630f3e1ed20e42442d8261431672b501b1827504
noelgarber <104410131+noelgarber@users.noreply.github.com>
2022-09-29 14:37:33 -0400

    Update PACM_General_Vars.py

--- PACM_General_Vars.py
+++ PACM_General_Vars.py
@@ -1,5 +1,7 @@
 #General reference objects
 
+#Define chemical characteristics of amino acids. This could conceivably be adjusted, but we have not tested it. 
+#We group amino acids by characteristic because processing them individually in Step2_Pairwise_SLiM_Matrices_Generator.py would require a much larger input dataset (on the order of thousands of peptides).
 aa_charac_dict = {
 	"Acidic": ["D", "E"],
 	"Basic": ["K", "R", "H"],
@@ -11,7 +13,32 @@ aa_charac_dict = {
 	"Special_Cases": ["P", "G"]
 }
 
+#General list of proteinogenic amino acids
 list_aa_no_phos = ["D", "E", "R", "H", "K", "S", "T", "N", "Q", "C", "G", "P", "A", "V", "I", "L", "M", "F", "Y", "W"]
 
+#List of amino acids that includes pSer (assigned as "B"), pThr (assigned as "J"), and pTyr (assigned as "O"). The single letter codes for phospho-residues are arbitrary. 
 list_aa = ["D", "E", "R", "H", "K", "S", "T", "N", "Q", "C", "G", "P", "A", "V", "I", "L", "M", "F", "Y", "W", "B", "J", "O"]
-#We had to assign single letter codes to phosphorylated residues, so we used B = pSer, J = pThr, and O = pTyr..
+
+#Dict of amino acids with their row indices for lookup in position-weighted matrices. Arbitrary, but must be universal between scripts. 
+index_aa_dict = {
+	"D": 0,
+	"E": 1,
+	"R": 2,
+	"H": 3,
+	"K": 4,
+	"S": 5,
+	"T": 6,
+	"N": 7,
+	"Q": 8,
+	"C": 9,
+	"G": 10,
+	"P": 11,
+	"A": 12,
+	"V": 13,
+	"I": 14,
+	"L": 15,
+	"M": 16,
+	"F": 17,
+	"Y": 18,
+	"W": 19
+}
