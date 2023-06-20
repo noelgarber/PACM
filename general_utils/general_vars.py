48101b4a97ddc912c04637e50d0d50bd35d18537
noelgarber <104410131+noelgarber@users.noreply.github.com>
2023-06-19 23:27:49 -0400

    General variables (e.g. amino acid lists, chemical characteristics, etc.)

---
+++ general_utils/general_vars.py
@@ -0,0 +1,16 @@
+# Declare the amino acid chemical characteristics dictionary
+aa_charac_dict = {
+    "Acidic": ["D", "E"],
+    "Basic": ["K", "R", "H"],
+    "ST": ["S", "T"],
+    "Aromatic": ["F", "Y"],
+    "Aliphatic": ["A", "V", "I", "L"],
+    "Other_Hydrophobic": ["W", "M"],
+    "Polar_Uncharged": ["N", "Q", "C"],
+    "Special_Cases": ["P", "G"]
+}
+
+# Declare the sorted list of amino acids
+amino_acids = ("D", "E", "R", "H", "K", "S", "T", "N", "Q", "C", "G", "P", "A", "V", "I", "L", "M", "F", "Y", "W")
+amino_acids_phos = ("D", "E", "R", "H", "K", "S", "T", "N", "Q", "C", "G", "P", "A", "V", "I", "L", "M", "F", "Y", "W", "B", "J", "O") # B=pSer, J=pThr, Y=pTyr
+
