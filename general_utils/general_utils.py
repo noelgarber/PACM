a05a5a024f4d7c0d7d97a62a24caa244a3f06d46
noelgarber <104410131+noelgarber@users.noreply.github.com>
2023-06-24 17:13:20 -0400

    Moved permute_weights() to weights_utils.py

--- general_utils/general_utils.py
+++ general_utils/general_utils.py
@@ -1,6 +1,5 @@
 import csv
 import os
-from user_helper_functions import get_position_copies
 
 def input_number(prompt, mode = "float"):
     '''
@@ -121,7 +120,7 @@ def get_delimited_list(prompt, enforced_list_length = None, delimiter = ",", dty
             delimited_list = [dtype(x) for x in delimited_list]
             if not enforced_list_length:
                 valid_list_given = True
-            elif len(delimited_list) = enforced_list_length:
+            elif len(delimited_list) == enforced_list_length:
                 valid_list_given = True
             else:
                 print(f"Incorrect list length (expected {enforced_list_length}, got {len(delimited_list)}); please try again.")
@@ -148,45 +147,6 @@ def mean_at_index(df, row_index, col_names):
     mean_value = values.mean()
     return mean_value
 
-def permute_weights(slim_length, position_copies = None):
-    '''
-    Simple function to generate permutations of weights from 0-3 for scoring a peptide motif of defined length
-
-    Args:
-        slim_length (int): 	    the length of the motif for which weights are being generated
-        position_copies (dict): a dictionary of position --> copy_num, where the sum of dict values equals slim_length
-
-    Returns:
-        expanded_weights_array (np.ndarray): an array of shape (permutations_number, slim_length)
-    '''
-
-    if position_copies is None:
-        position_copies = get_position_copies(slim_length)
-
-    # Check that the dictionary of position copies is the correct length
-    if slim_length != sum(position_copies.values()):
-        raise ValueError(f"permute_weights error: slim_length ({slim_length}) is not equal to position_copies dict values sum ({sum(position_copies.values())})")
-
-    # Get permutations of possible weights at each position
-    permutations_length = slim_length
-    for position, total_copies in position_copies.items():
-        permutations_length = permutations_length - (total_copies - 1)
-    permuted_weights = np.array(np.meshgrid(*([[3, 2, 1, 0]] * permutations_length))).T.reshape(-1, permutations_length)
-
-    # Expand permutations to copied columns
-    expanded_weights_list = []
-    for position, total_copies in position_copies.items():
-        current_column = permuted_weights[:, position:position + 1]
-        if total_copies == 1:
-            expanded_weights_list.append(current_column)
-        else:
-            repeated_column = np.repeat(current_column, total_copies, axis=1)
-            expanded_weights_list.append(repeated_column)
-    expanded_weights_array = np.hstack(expanded_weights_list)
-
-    print(f"Shape of expanded_weights_array: {expanded_weights_array.shape}")
-
-    return expanded_weights_array
 
 def save_dict(dictionary, output_path, filename):
     # Basic function to save key-value pairs as lines in a text file
---
+++ general_utils/weights_utils.py
@@ -0,0 +1,41 @@
+from general_utils.user_helper_functions import get_position_copies
+
+def permute_weights(slim_length, position_copies = None):
+    '''
+    Simple function to generate permutations of weights from 0-3 for scoring a peptide motif of defined length
+
+    Args:
+        slim_length (int): 	    the length of the motif for which weights are being generated
+        position_copies (dict): a dictionary of position --> copy_num, where the sum of dict values equals slim_length
+
+    Returns:
+        expanded_weights_array (np.ndarray): an array of shape (permutations_number, slim_length)
+    '''
+
+    if position_copies is None:
+        position_copies = get_position_copies(slim_length)
+
+    # Check that the dictionary of position copies is the correct length
+    if slim_length != sum(position_copies.values()):
+        raise ValueError(f"permute_weights error: slim_length ({slim_length}) is not equal to position_copies dict values sum ({sum(position_copies.values())})")
+
+    # Get permutations of possible weights at each position
+    permutations_length = slim_length
+    for position, total_copies in position_copies.items():
+        permutations_length = permutations_length - (total_copies - 1)
+    permuted_weights = np.array(np.meshgrid(*([[3, 2, 1, 0]] * permutations_length))).T.reshape(-1, permutations_length)
+
+    # Expand permutations to copied columns
+    expanded_weights_list = []
+    for position, total_copies in position_copies.items():
+        current_column = permuted_weights[:, position:position + 1]
+        if total_copies == 1:
+            expanded_weights_list.append(current_column)
+        else:
+            repeated_column = np.repeat(current_column, total_copies, axis=1)
+            expanded_weights_list.append(repeated_column)
+    expanded_weights_array = np.hstack(expanded_weights_list)
+
+    print(f"Shape of expanded_weights_array: {expanded_weights_array.shape}")
+
+    return expanded_weights_array
