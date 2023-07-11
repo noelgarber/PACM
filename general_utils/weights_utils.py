a6ac55855fce7d46fcb01f7ae7f83b189d4ff04b
noelgarber <104410131+noelgarber@users.noreply.github.com>
2023-07-10 20:52:55 -0400

    Changed method for permuting weights

--- general_utils/weights_utils.py
+++ general_utils/weights_utils.py
@@ -1,42 +1,28 @@
 import numpy as np
-from general_utils.user_helper_functions import get_position_copies
+from general_utils.user_helper_functions import get_possible_weights
 
-def permute_weights(slim_length, position_copies = None):
+def permute_weights(slim_length, possible_weights = None):
     '''
     Simple function to generate permutations of weights from 0-3 for scoring a peptide motif of defined length
 
     Args:
-        slim_length (int): 	    the length of the motif for which weights are being generated
-        position_copies (dict): a dictionary of position --> copy_num, where the sum of dict values equals slim_length
+        slim_length (int): 	     the length of the motif for which weights are being generated
+        possible_weights (list): a list of numpy arrays with possible weights at each position for permutation
 
     Returns:
-        expanded_weights_array (np.ndarray): an array of shape (permutations_number, slim_length)
+        result (np.ndarray):     an array of shape (permutations_number, slim_length)
     '''
 
-    if position_copies is None:
-        position_copies = get_position_copies(slim_length)
+    if possible_weights is None:
+        possible_weights = get_possible_weights(slim_length)
 
-    # Check that the dictionary of position copies is the correct length
-    if slim_length != sum(position_copies.values()):
-        raise ValueError(f"permute_weights error: slim_length ({slim_length}) is not equal to position_copies dict values sum ({sum(position_copies.values())})")
+    permutations_count = np.prod([len(arr) for arr in possible_weights])
 
-    # Get permutations of possible weights at each position
-    permutations_length = slim_length
-    for position, total_copies in position_copies.items():
-        permutations_length = permutations_length - (total_copies - 1)
-    permuted_weights = np.array(np.meshgrid(*([[3, 2, 1, 0.5, 0]] * permutations_length))).T.reshape(-1, permutations_length)
+    result = np.empty((permutations_count, slim_length), dtype=np.int)
+    meshgrid_arrays = np.meshgrid(*possible_weights)
+    for i, arr in enumerate(meshgrid_arrays):
+        result[:, i] = arr.ravel()
 
-    # Expand permutations to copied columns
-    expanded_weights_list = []
-    for position, total_copies in position_copies.items():
-        current_column = permuted_weights[:, position:position + 1]
-        if total_copies == 1:
-            expanded_weights_list.append(current_column)
-        else:
-            repeated_column = np.repeat(current_column, total_copies, axis=1)
-            expanded_weights_list.append(repeated_column)
-    expanded_weights_array = np.hstack(expanded_weights_list)
+    print(f"Shape of permuted weights array: {result.shape}")
 
-    print(f"Shape of expanded_weights_array: {expanded_weights_array.shape}")
-
-    return expanded_weights_array
+    return result
