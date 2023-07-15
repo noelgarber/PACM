61f6cab1dccb66a694ecc170d1f656d927026564
noelgarber <104410131+noelgarber@users.noreply.github.com>
2023-07-15 02:15:28 -0400

    Added aa_charac_dict and general_params

--- Matrix_Generator/config.py
+++ Matrix_Generator/config.py
@@ -4,7 +4,21 @@ from general_utils.general_vars import amino_acids_phos
 
 ''' ----------------------------------------------------------------------------------------------------------------
                                     Conditional Weighted Matrix Configuration
-                                    
+    
+    Structure of aa_charac_dict: 
+        chemical_characteristic: list of member amino acids
+        Example: 
+            "Acidic": ["D","E"]
+    
+    Structure of general_params: 
+        "motif_length":           length of the peptide motif for which matrices are being generated
+        "output_folder":          folder to save matrices and scored data into; user is prompted if None
+        "make_calls":             whether to make calls on whether peptides pass based on scoring
+        "aa_charac_dict":         the aforementioned amino acid side chain characteristics dictionary
+        "convert_phospho":        whether to convert phospho-residues in peptide before constructing matrices; 
+                                  phospho-residues are denoted as pSer=B, pThr=J, pTyr=O
+        "position_thresholds":    range of values for optimization, as a list, for thresholding points values
+    
     Structure of data_params: 
         "bait":                   bait protein name for generating conditional matrices; if None, best is used
         "bait_signal_col_marker": keyword denoting whether a column in the input dataframe has bait signal values
@@ -31,6 +45,24 @@ from general_utils.general_vars import amino_acids_phos
         "sigmoid_inflection":     threshold where matrix values are scaled larger when above or smaller when below
         
     ---------------------------------------------------------------------------------------------------------------- '''
+
+aa_charac_dict = {"Acidic": ["D", "E"],
+                  "Basic": ["K", "R"],
+                  "ST": ["S", "T", "B", "J"],
+                  "Aromatic": ["F", "Y", "W", "O"],
+                  "Aliphatic": ["A", "V", "I", "L", "M"],
+                  "Polar": ["N", "Q", "H", "C"],
+                  "Proline": ["P"],
+                  "Glycine": ["G"]
+}
+
+general_params = {"motif_length": 15,
+                  "output_folder": None,
+                  "make_calls": True,
+                  "aa_charac_dict": aa_charac_dict,
+                  "convert_phospho": True,
+                  "position_thresholds": np.array([0.4,0.8,0.0,0.6,0.2])}
+
 data_params = {"bait": None,
                "bait_signal_col_marker": "Background-Adjusted_Standardized_Signal",
                "best_signal_col": "Max_Bait_Background-Adjusted_Mean",
