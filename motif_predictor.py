a044804b95e4f2ac1e67b35a7180d74ccca843c5
noelgarber <104410131+noelgarber@users.noreply.github.com>
2023-08-06 13:39:21 -0400

    Added motif topology prediction

--- motif_predictor.py
+++ motif_predictor.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from Motif_Predictor.score_protein_motifs import score_protein_seqs
 from Motif_Predictor.specificity_score_assigner import apply_specificity_scores
+from Motif_Predictor.motif_topology_predictor import predict_topology
 from Motif_Predictor.predictor_config import predictor_params
 
 def main(predictor_params = predictor_params):
@@ -34,6 +35,10 @@ def main(predictor_params = predictor_params):
     # Save scored data
     output_path = predictor_params["scored_output_path"]
     protein_seqs_df.to_csv(output_path)
+    print(f"Saved scored motifs to {output_path}")
+
+    # Get topology for predicted motifs
+    protein_seqs_df = predict_topology(protein_seqs_df, motif_seq_cols, predictor_params)
 
     return protein_seqs_df
 
