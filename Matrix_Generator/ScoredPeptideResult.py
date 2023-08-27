125674e0538c0e87ee8fc318d9f1a0c6aaa3bec9
noelgarber <104410131+noelgarber@users.noreply.github.com>
2023-08-26 20:34:10 -0400

    Added aa chemical groups as NN-trainable attributes

--- Matrix_Generator/ScoredPeptideResult.py
+++ Matrix_Generator/ScoredPeptideResult.py
@@ -4,19 +4,20 @@ import numpy as np
 import pandas as pd
 import warnings
 try:
-    from Matrix_Generator.config_local import data_params, matrix_params, aa_equivalence_dict
+    from Matrix_Generator.config_local import aa_charac_dict
 except:
-    from Matrix_Generator.config import data_params, matrix_params, aa_equivalence_dict
+    from Matrix_Generator.config import aa_charac_dict
 
 class ScoredPeptideResult:
     '''
     Class that represents the result of scoring peptides using ConditionalMatrices.score_peptides()
     '''
-    def __init__(self, slice_scores_subsets, positive_scores_2d, suboptimal_scores_2d, forbidden_scores_2d):
+    def __init__(self, seqs_2d, slice_scores_subsets, positive_scores_2d, suboptimal_scores_2d, forbidden_scores_2d):
         '''
         Initialization function to generate the score values and assign them to self
 
         Args:
+            seqs_2d  (np.ndarray):             2D array of single letter code amino acids, where each row is a peptide
             slice_scores_subsets (np.ndarray): array of span lengths in the motif to stratify scores by; e.g. if it is
                                                [6,7,2], then subset scores are derived for positions 1-6, 7:13, & 14:15
             positive_scores_2d (np.ndarray):   standardized predicted signal values for each residue for each peptide
@@ -29,6 +30,9 @@ class ScoredPeptideResult:
             if slice_scores_subsets.sum() != positive_scores_2d.shape[1]:
                 raise ValueError(f"ScoredPeptideResult error: slice_scores_subsets sum ({slice_scores_subsets.sum()}) "
                                  f"does not match axis=1 shape of 2D score arrays ({positive_scores_2d.shape[1]})")
+
+        # Assign constituent sequences to self
+        self.sequences_2d = seqs_2d
 
         # Assign predicted signals score values
         self.positive_scores_2d = positive_scores_2d
@@ -48,6 +52,9 @@ class ScoredPeptideResult:
         self.slice_scores_subsets = slice_scores_subsets
         self.slice_scores()
         self.stack_scores()
+
+        # Encode source residues with integer values; these will be used for NN training
+        self.encode_residues(aa_charac_dict)
 
     def slice_scores(self):
         # Function that generates scores based on sliced subsets of peptide sequences
@@ -101,3 +108,15 @@ class ScoredPeptideResult:
         # Stack the scores and also use them to construct a dataframe
         self.stacked_scores = np.stack(scores).T
         self.scored_df = pd.DataFrame(self.stacked_scores, columns = self.score_cols)
+
+    def encode_residues(self, aa_charac_dict = aa_charac_dict):
+        '''
+        Function that creates an encoded representation of sequences by chemical group
+        '''
+
+        self.charac_arrays = {}
+        for charac, member_list in aa_charac_dict.items():
+            is_member = np.isin(self.sequences_2d, member_list)
+            self.charac_arrays[charac] = is_member
+
+        self.stacked_encoded_characs = np.hstack(list(self.charac_arrays.values()))
