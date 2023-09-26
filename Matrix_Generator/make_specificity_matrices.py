8b256e116640664b650af6fc3faeb384cb2b3a94
noelgarber <104410131+noelgarber@users.noreply.github.com>
2023-09-26 02:12:36 -0400

    Added option to combine f1-score and linear R2 in a maximizable function

--- Matrix_Generator/make_specificity_matrices.py
+++ Matrix_Generator/make_specificity_matrices.py
@@ -17,14 +17,14 @@ except:
                      Define functions for parallelized optimization of specificity matrix weights
    ------------------------------------------------------------------------------------------------------------------'''
 
-def process_weights_chunk(chunk, specificity_matrix, use_r2 = True):
+def process_weights_chunk(chunk, specificity_matrix, fit_mode = "both"):
     '''
     Lower helper function for parallelization of position weight optimization for the specificity matrix
 
     Args:
         chunk (np.ndarray):                     the chunk of permuted/randomized weights currently being processed
         specificity_matrix (SpecificityMatrix): the specificity matrix object
-        use_r2 (bool):                          whether to maximize R2; if false, f1-score is maximized instead
+        fit_mode (str):                         whether to use linear R2, f1-score, or both (as a mean of R2 * f1)
 
     Returns:
         chunk_results (tuple):                  (optimized_weights, optimized_mean_f1)
@@ -43,7 +43,15 @@ def process_weights_chunk(chunk, specificity_matrix, use_r2 = True):
         specificity_matrix.set_specificity_statistics(use_weighted = True)
         current_mean_f1 = specificity_matrix.weighted_mean_f1
         current_r2 = specificity_matrix.weighted_linear_r2
-        current_maximizable_value = current_r2 if use_r2 else current_mean_f1
+        if fit_mode == "both":
+            current_maximizable_value = (current_r2 + current_mean_f1) / 2
+        elif fit_mode == "R2" or fit_mode == "r2":
+            current_maximizable_value = current_r2
+        elif fit_mode == "f1":
+            current_maximizable_value = current_mean_f1
+        else:
+            raise ValueError(f"process_weights_chunk got fit_mode={fit_mode}, but must be `both`, `R2`, or `f1`")
+
         if current_maximizable_value > maximizable_value:
             maximizable_value = current_maximizable_value
             optimized_mean_f1 = current_mean_f1
@@ -52,14 +60,14 @@ def process_weights_chunk(chunk, specificity_matrix, use_r2 = True):
 
     return (maximizable_value, optimized_mean_f1, optimized_r2, optimized_weights)
 
-def process_weights(weights_array_chunks, specificity_matrix, use_r2 = True):
+def process_weights(weights_array_chunks, specificity_matrix, fit_mode = "both"):
     '''
     Upper helper function for parallelization of position weight optimization; processes weights by chunking
 
     Args:
         weights_array_chunks (list):            list of chunks as numpy arrays for feeding to process_weights_chunk
         specificity_matrix (SpecificityMatrix): the specificity matrix object
-        use_r2 (bool):                          whether to maximize R2; if false, f1-score is maximized instead
+        fit_mode (str):                         whether to use linear R2, f1-score, or both (as a mean of R2 * f1)
 
     Returns:
         results (tuple):                        (best_weights, best_mean_f1)
@@ -74,7 +82,7 @@ def process_weights(weights_array_chunks, specificity_matrix, use_r2 = True):
 
     # Set up the parallel processing operation
     pool = multiprocessing.Pool()
-    process_partial = partial(process_weights_chunk, specificity_matrix = specificity_matrix, use_r2 = use_r2)
+    process_partial = partial(process_weights_chunk, specificity_matrix = specificity_matrix, fit_mode = fit_mode)
 
     best_maximizable_value = 0
     best_mean_f1 = 0
@@ -86,10 +94,7 @@ def process_weights(weights_array_chunks, specificity_matrix, use_r2 = True):
             if chunk_results[0] > best_maximizable_value:
                 best_maximizable_value, best_mean_f1, best_r2, best_weights = chunk_results
                 formatted_weights = ", ".join(best_weights.round(2).astype(str))
-                if use_r2:
-                    print(f"\nNew record: linear R2={best_r2} (f1={best_mean_f1}) for weights: [{formatted_weights}]")
-                else:
-                    print(f"\nNew record: f1={best_mean_f1} (linear R2={best_r2}) for weights: [{formatted_weights}]")
+                print(f"\nNew record: f1={best_mean_f1} and linear R2={best_r2}) for weights: [{formatted_weights}]")
 
             pbar.update()
 
@@ -98,7 +103,8 @@ def process_weights(weights_array_chunks, specificity_matrix, use_r2 = True):
 
     return (best_weights, best_mean_f1, best_r2)
 
-def find_optimal_weights(specificity_matrix, motif_length, chunk_size = 1000, ignore_positions = None, use_r2 = True):
+def find_optimal_weights(specificity_matrix, motif_length, chunk_size = 1000, ignore_positions = None,
+                         fit_mode = "both"):
     '''
     Parent function for finding optimal position weights to generate an optimally weighted specificity matrix
 
@@ -107,14 +113,14 @@ def find_optimal_weights(specificity_matrix, motif_length, chunk_size = 1000, ig
         motif_length (int):                     length of the motif being studied
         chunk_size (int):                       the number of position weights to process at a time
         ignore_positions (iterable):            positions to force to 0 for weights arrays
-        use_r2 (bool):                          whether to maximize R2; if false, f1-score is maximized instead
+        fit_mode (str):                         whether to use linear R2, f1-score, or both (as a mean of R2 * f1)
 
     Returns:
         specificity_matrix (SpecificityMatrix): the fitted SpecificityMatrix object containing matrices and scored data
     '''
 
     # Get the permuted weights and break into chunks for parallelization
-    sample_size = 1000000
+    sample_size = 10000000
     value_range = (0.0, 4.0)
     trial_weights = np.random.uniform(value_range[0], value_range[1], size=(sample_size, motif_length))
     if ignore_positions is not None:
@@ -127,7 +133,7 @@ def find_optimal_weights(specificity_matrix, motif_length, chunk_size = 1000, ig
     weights_array_chunks = [trial_weights[i:i + chunk_size] for i in range(0, len(trial_weights), chunk_size)]
 
     # Run the parallelized optimization process
-    results = process_weights(weights_array_chunks, specificity_matrix, use_r2)
+    results = process_weights(weights_array_chunks, specificity_matrix, fit_mode)
     best_weights, best_mean_f1, best_linear_r2 = results
 
     # Apply the final best weights onto the specificity matrix and score the source data
@@ -166,8 +172,8 @@ def main(source_df, comparator_info = comparator_info, specificity_params = spec
         length = specificity_params["motif_length"]
         chunk_size = specificity_params["chunk_size"]
         ignore_positions = specificity_params["ignore_positions"]
-        use_r2 = specificity_params["use_r2"]
-        specificity_matrix = find_optimal_weights(specificity_matrix, length, chunk_size, ignore_positions, use_r2)
+        fit_mode = specificity_params["fit_mode"]
+        specificity_matrix = find_optimal_weights(specificity_matrix, length, chunk_size, ignore_positions, fit_mode)
 
     # Save the results
     output_folder = specificity_params.get("output_folder")
