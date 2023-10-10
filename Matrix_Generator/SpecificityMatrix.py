Fixed error in upper and lower accuracy calculation

Matrix_Generator/SpecificityMatrix.py
@@ -52,7 +52,7 @@ def optimize_accuracy(actual_labels, score_values):
     valid_total_matches = np.equal(valid_predicted_labels, valid_actual_labels)
     best_total_accuracy = valid_total_matches.mean()
 
-    return (best_total_accuracy, best_above_threshold, best_below_threshold)
+    return (best_total_accuracy, best_above_accuracy, best_below_accuracy, best_above_threshold, best_below_threshold)
 
 def optimize_mcc(actual_truths, score_values):
     # Helper function that finds the best MCC at the optimal threshold for numerical scores in a binary classification
@@ -469,14 +469,19 @@ class SpecificityMatrix:
             multiclass_labels = np.zeros(shape=log2fc_above_upper.shape)
             multiclass_labels[log2fc_above_upper] = 1
             multiclass_labels[log2fc_below_lower] = 2
-            best_accuracy, best_thres_upper, best_thres_lower = optimize_accuracy(multiclass_labels, valid_score_values)
+            result = optimize_accuracy(multiclass_labels, valid_score_values)
+            best_accuracy, best_upper_accuracy, best_lower_accuracy, best_thres_upper, best_thres_lower = result
 
             if use_weighted:
                 self.weighted_upper_threshold, self.weighted_lower_threshold = best_thres_upper, best_thres_lower
                 self.weighted_accuracy = best_accuracy
+                self.weighted_upper_accuracy = best_upper_accuracy
+                self.weighted_lower_accuracy = best_lower_accuracy
             else:
                 self.unweighted_upper_threshold, self.unweighted_lower_threshold = best_thres_upper, best_thres_lower
                 self.unweighted_accuracy = best_accuracy
+                self.unweighted_upper_accuracy = best_upper_accuracy
+                self.unweighted_lower_accuracy = best_lower_accuracy
 
         elif statistic_type == "mcc":
             # Find upper and lower MCC values
@@ -497,6 +502,8 @@ class SpecificityMatrix:
             actual_classes[log2fc_above_upper] = 1
             actual_classes[log2fc_below_lower] = 2
 
+            upper_accuracy = np.equal(log2fc_above_upper, predicted_upper).mean()
+            lower_accuracy = np.equal(log2fc_below_lower, predicted_lower).mean()
             accuracy = np.equal(predicted_classes, actual_classes).mean()
 
             if use_weighted:
@@ -504,11 +511,13 @@ class SpecificityMatrix:
                 self.weighted_upper_mcc, self.weighted_lower_mcc = best_mcc_upper, best_mcc_lower
                 self.weighted_upper_threshold, self.weighted_lower_threshold = best_thres_upper, best_thres_lower
                 self.weighted_accuracy = accuracy
+                self.weighted_upper_accuracy, self.weighted_lower_accuracy = upper_accuracy, lower_accuracy
             else:
                 self.unweighted_mean_mcc = weighted_mean_mcc
                 self.unweighted_upper_mcc, self.unweighted_lower_mcc = best_mcc_upper, best_mcc_lower
                 self.unweighted_upper_threshold, self.unweighted_lower_threshold = best_thres_upper, best_thres_lower
                 self.unweighted_accuracy = accuracy
+                self.unweighted_upper_accuracy, self.unweighted_lower_accuracy = upper_accuracy, lower_accuracy
 
         elif statistic_type == "f1":
             # Find upper and lower f1-scores
@@ -529,6 +538,8 @@ class SpecificityMatrix:
             actual_classes[log2fc_above_upper] = 1
             actual_classes[log2fc_below_lower] = 2
 
+            upper_accuracy = np.equal(log2fc_above_upper, predicted_upper).mean()
+            lower_accuracy = np.equal(log2fc_below_lower, predicted_lower).mean()
             accuracy = np.equal(predicted_classes, actual_classes).mean()
 
             if use_weighted:
@@ -536,11 +547,13 @@ class SpecificityMatrix:
                 self.weighted_upper_f1, self.weighted_lower_f1 = best_f1_upper, best_f1_lower
                 self.weighted_upper_threshold, self.weighted_lower_threshold = best_thres_upper, best_thres_lower
                 self.weighted_accuracy = accuracy
+                self.weighted_upper_accuracy, self.weighted_lower_accuracy = upper_accuracy, lower_accuracy
             else:
                 self.unweighted_mean_f1 = weighted_mean_f1
                 self.unweighted_upper_f1, self.unweighted_lower_f1 = best_f1_upper, best_f1_lower
                 self.unweighted_upper_threshold, self.unweighted_lower_threshold = best_thres_upper, best_thres_lower
                 self.unweighted_accuracy = accuracy
+                self.unweighted_upper_accuracy, self.unweighted_lower_accuracy = upper_accuracy, lower_accuracy
 
         # Also find R2 of a linear function relating log2fc to specificity score
         r2_value, linear_model = linear_regression(valid_score_values, valid_log2fc_values)

Matrix_Generator/make_specificity_matrices.py
@@ -63,7 +63,14 @@ def process_weights_chunk(chunk, specificity_matrix, fit_mode = "f1", side = Non
         specificity_matrix.set_specificity_statistics(use_weighted=True, statistic_type="f1")
         optimized_r2 = specificity_matrix.weighted_linear_r2
         optimized_f1 = specificity_matrix.weighted_mean_f1
-        optimized_accuracy = specificity_matrix.weighted_accuracy
+
+        if side == "upper":
+            optimized_accuracy = specificity_matrix.weighted_upper_accuracy
+        elif side == "lower":
+            optimized_accuracy = specificity_matrix.weighted_lower_accuracy
+        else:
+            optimized_accuracy = specificity_matrix.weighted_accuracy
+
         maximizable_value = optimized_accuracy
 
     elif fit_mode == "MCC" or fit_mode == "mcc":
@@ -94,7 +101,13 @@ def process_weights_chunk(chunk, specificity_matrix, fit_mode = "f1", side = Non
         optimized_r2 = specificity_matrix.weighted_linear_r2
         optimized_f1 = np.nan
         maximizable_value = optimized_mcc
-        optimized_accuracy = specificity_matrix.weighted_accuracy
+
+        if side == "upper":
+            optimized_accuracy = specificity_matrix.weighted_upper_accuracy
+        elif side == "lower":
+            optimized_accuracy = specificity_matrix.weighted_lower_accuracy
+        else:
+            optimized_accuracy = specificity_matrix.weighted_accuracy
 
     elif fit_mode == "f1":
         for weights in chunk:
@@ -124,7 +137,13 @@ def process_weights_chunk(chunk, specificity_matrix, fit_mode = "f1", side = Non
         optimized_r2 = specificity_matrix.weighted_linear_r2
         optimized_mcc = np.nan
         maximizable_value = optimized_f1
-        optimized_accuracy = specificity_matrix.weighted_accuracy
+
+        if side == "upper":
+            optimized_accuracy = specificity_matrix.weighted_upper_accuracy
+        elif side == "lower":
+            optimized_accuracy = specificity_matrix.weighted_lower_accuracy
+        else:
+            optimized_accuracy = specificity_matrix.weighted_accuracy
 
     elif fit_mode == "R2":
         for weights in chunk:
@@ -148,7 +167,13 @@ def process_weights_chunk(chunk, specificity_matrix, fit_mode = "f1", side = Non
         optimized_f1 = specificity_matrix.weighted_mean_f1
         optimized_mcc = np.nan
         maximizable_value = optimized_r2
-        optimized_accuracy = specificity_matrix.weighted_accuracy
+
+        if side == "upper":
+            optimized_accuracy = specificity_matrix.weighted_upper_accuracy
+        elif side == "lower":
+            optimized_accuracy = specificity_matrix.weighted_lower_accuracy
+        else:
+            optimized_accuracy = specificity_matrix.weighted_accuracy
 
     else:
         raise ValueError(f"process_weights_chunk got fit_mode={fit_mode}, but must be `mcc`, `f1`, or `R2`")
@@ -210,8 +235,18 @@ def process_weights(weights_array_chunks, specificity_matrix, fit_mode = "f1", s
             better_std = chunk_results[0] == best_maximizable_value and chunk_results[6] < best_weights_std
             if better_value or better_std:
                 best_maximizable_value, best_accuracy, best_mcc, best_f1, best_r2, best_weights, best_weights_std = chunk_results
-                formatted_weights = ", ".join(best_weights.round(2).astype(str))
-                print(f"\nNew record: {side} {fit_mode} = {best_maximizable_value} (overall accuracy = {best_accuracy}) for weights: [{formatted_weights}] (SD = {best_weights_std})")
+                weights_str = ", ".join(best_weights.round(2).astype(str))
+
+                if side is not None:
+                    max_str = f"{side} {fit_mode} = {best_maximizable_value}"
+                else:
+                    max_str = f"{fit_mode} = {best_maximizable_value}"
+
+                if fit_mode == "accuracy":
+                    print(f"\nNew record: {max_str} for weights: [{weights_str}] (SD = {best_weights_std})")
+                else:
+                    acc_str = f"{side} accuracy = {best_accuracy}" if side is not None else f"accuracy = {best_accuracy}"
+                    print(f"\nNew record: {max_str} ({acc_str}) for weights: [{weights_str}] (SD = {best_weights_std})")
 
             pbar.update()
 