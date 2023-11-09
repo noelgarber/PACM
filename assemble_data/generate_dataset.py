568c26e1acf7c3ea927dee3e5881ede725b8f66f
noelgarber <104410131+noelgarber@users.noreply.github.com>
2023-11-09 00:57:32 -0500

    Fixed sequence retrieval method

--- assemble_data/generate_dataset.py
+++ assemble_data/generate_dataset.py
@@ -24,30 +24,26 @@ def fetch_accessions(dataset_name = "hsapiens_gene_ensembl"):
     dataset = server.datasets[dataset_name]
 
     # Get the Ensembl and NCBI accession numbers
-    main_attributes = ["ensembl_gene_id", "ensembl_transcript_id", "ensembl_peptide_id",
-                       "external_gene_name", "gene_biotype", "refseq_peptide", "refseq_peptide_predicted"]
-    col_names = main_attributes.copy()
+    attributes = ["ensembl_gene_id", "ensembl_transcript_id", "ensembl_peptide_id",
+                  "external_gene_name", "gene_biotype", "refseq_peptide", "refseq_peptide_predicted"]
     print("Requesting Ensembl and NCBI accessions...")
-    main_response = dataset.search({"attributes": main_attributes})
+    response = dataset.search({"attributes": attributes})
 
     print("\tReceived! Parsing data...")
-    main_response_lines = [line.decode("utf-8") for line in main_response.iter_lines()]
-    main_data_rows = [row.split("\t") for row in main_response_lines if row]
-    accessions_df = pd.DataFrame(main_data_rows, columns=col_names)
+    lines = response.text.split("\n")
+    rows = [row.split("\t") for row in lines if row]
+    accessions_df = pd.DataFrame(rows, columns=attributes)
 
     # Get the Uniprot accession numbers separately (requesting with NCBI accessions results in an error)
     uniprot_attributes = ["ensembl_peptide_id", "uniprotswissprot", "uniprotsptrembl"]
     print("Requesting UniProt accessions...")
     uniprot_response = dataset.search({"attributes": uniprot_attributes})
-    uniprot_response_lines = [line.decode("utf-8") for line in uniprot_response.iter_lines()]
-    uniprot_dict = {}
-    trembl_dict = {}
+    uniprot_lines = uniprot_response.text.split("\n")
+
     print("\tReceived! Parsing data...")
-    for row in uniprot_response_lines:
-        if row:
-            ensembl_id, uniprot_id, trembl_id = row.split("\t")
-            uniprot_dict[ensembl_id] = uniprot_id
-            trembl_dict[ensembl_id] = trembl_id
+    uniprot_rows = [row.split("\t") for row in uniprot_lines if row]
+    uniprot_dict = {row[0]: row[1] for row in uniprot_rows if row}
+    trembl_dict = {row[0]: row[2] for row in uniprot_rows if row}
 
     # Merge data
     print("Merging data...")
@@ -56,7 +52,7 @@ def fetch_accessions(dataset_name = "hsapiens_gene_ensembl"):
     matching_trembl_ids = [trembl_dict.get(ensembl_id) for ensembl_id in ensembl_peptide_ids]
     accessions_df["uniprot"] = matching_uniprot_ids
     accessions_df["trembl"] = matching_trembl_ids
-    print("Success! Accessions received.")
+    print("Done! Accessions were successfully retrieved.")
 
     return accessions_df
 
@@ -80,17 +76,30 @@ def fetch_seqs(dataset_name = "hsapiens_gene_ensembl"):
     attributes = ["ensembl_peptide_id", "peptide"]
     print("Requesting sequences...")
     response = dataset.search({"attributes": attributes})
-    fasta_data = response.text
 
-    # Parse the FASTA data
-    sequence_data = {}
-    print("Parsing sequence data...")
-    for record in SeqIO.parse(StringIO(fasta_data), "fasta"):
-        sequence_data[record.id] = str(record.seq)
+    print("\tParsing data...")
+    lines = response.text.split("\n")
+    rows = [line.split("\t") for line in lines if line]
+    first_row = rows[0]
+    if first_row[0][0:4] == "ENSP":
+        sequence_data = {row[0]: row[1] for row in rows}
+    else:
+        sequence_data = {row[1]: row[0] for row in rows}
+    print("Done! Sequences were successfully retrieved.")
 
     return sequence_data
 
 def generate_dataset(accession_dataset_name = "hsapiens_gene_ensembl", sequence_dataset_name = "hsapiens_gene_ensembl"):
+    '''
+    Main function that generates the dataset
+
+    Args:
+        accession_dataset_name (str): name of the Biomart dataset containing your species' accession numbers
+        sequence_dataset_name (str):  name of the Biomart dataset containing your species' protein sequences
+
+    Returns:
+        data_df (pd.DataFrame): dataframe with the accessions and protein sequence data
+    '''
 
     data_df = fetch_accessions(accession_dataset_name)
     sequence_data = fetch_seqs(sequence_dataset_name)
