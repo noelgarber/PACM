a7f7350a4498680c4ff4b1befb84ae4fb2aba95a
noelgarber <104410131+noelgarber@users.noreply.github.com>
2023-11-12 23:24:26 -0500

    Added default saving directory as parent folder of working directory

--- assemble_data/generate_dataset.py
+++ assemble_data/generate_dataset.py
@@ -161,7 +161,10 @@ if __name__ == "__main__":
     saved = False
     while not saved:
         try:
-            save_folder = input("Success! Enter the folder to save the data to:  ")
+            save_folder = input("Success! Enter the folder for saving data (or leave blank to use current directory:  ")
+            if save_folder == "": 
+                save_folder = os.getcwd()
+                save_folder = save_folder.rsplit("/",1)[0]
             save_path = os.path.join(save_folder, "dataset.csv")
             data_df.to_csv(save_path)
             saved = True
