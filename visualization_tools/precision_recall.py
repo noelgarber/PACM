a994000e257bec98bed1f95c6d049188147662a0
noelgarber <104410131+noelgarber@users.noreply.github.com>
2024-01-16 15:16:41 -0500

    Added horizontal and vertical dotted lines to help with precise thresholding

--- visualization_tools/precision_recall.py
+++ visualization_tools/precision_recall.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_precision_recall(precisions, recalls, accuracies, thresholds, save_path = None):
@@ -16,15 +17,30 @@ def plot_precision_recall(precisions, recalls, accuracies, thresholds, save_path
         None
     '''
 
+    top_accuracy_idx = np.nanargmax(accuracies)
+    top_threshold = thresholds[top_accuracy_idx]
+    top_accuracy = accuracies[top_accuracy_idx]
+    top_precision = precisions[top_accuracy_idx]
+    top_recall = recalls[top_accuracy_idx]
+
     if save_path is not None:
         save_folder = save_path.rsplit("/",1)[0]
         if not os.path.exists(save_folder):
             os.makedirs(save_folder)
 
-    plt.plot(thresholds, precisions, linestyle="-", label="Precision")
-    plt.plot(thresholds, recalls, linestyle="-", label="Recall")
-    plt.plot(thresholds, accuracies, linestyle="-", label="Accuracy")
+    plt.plot(thresholds, precisions, color="b", linestyle="-", label="Precision")
+    plt.plot(thresholds, recalls, color="r", linestyle="-", label="Recall")
+    plt.plot(thresholds, accuracies, color="g", linestyle="-", label="Accuracy")
+
+    plt.axhline(y=top_precision, color="b", linestyle='--', alpha=0.5)
+    plt.axhline(y=top_recall, color="r", linestyle='--', alpha=0.5)
+    plt.axhline(y=top_accuracy, color="g", linestyle='--', alpha=0.5)
+    plt.axvline(x=top_threshold, color="black", linestyle='--', alpha=0.5)
+
     plt.xlabel("Score Threshold")
     plt.legend()
+
     plt.savefig(save_path, format="pdf") if save_path is not None else None
     plt.show()
+
+    return (top_threshold, top_accuracy)
