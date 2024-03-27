0758a67a8efb2e0f721629eb23f43708044c3a77
noelgarber <104410131+noelgarber@users.noreply.github.com>
2024-03-27 18:00:46 -0400

    Create filter_ensembl_tm.py
    Filters transmembrane domains out of scannable protein sequences

---
+++ Motif_Predictor/filter_ensembl_tm.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+
+def parse_ensembl_tm(path):
+    '''
+    Function to parse Ensembl BioMart entries in a CSV/TSV that has Ensembl protein IDs and starts/ends for TMDs
+
+    Args:
+        path (str): path to the CSV/TSV containing the downloaded BioMart data
+
+    Returns:
+        ensembl_tm_dict (dict): dictionary of Ensembl protein ID --> list of (start,end) tuples
+    '''
+
+    if path.rsplit(".",1)[1] == "csv":
+        ensembl_tm_df = pd.read_csv(path)
+    else:
+        ensembl_tm_df = pd.read_csv(path, sep="\t")
+
+    ensembl_id_col = "ensembl_peptide_id" if "ensembl_peptide_id" in ensembl_tm_df.columns else "Protein stable ID"
+    start_col = "start" if "start" in ensembl_tm_df.columns else "Transmembrane helices start"
+    end_col = "end" if "end" in ensembl_tm_df.columns else "Transmembrane helices end"
+
+    ensembl_tm_df.dropna(subset=[start_col, end_col], inplace=True)
+    ensembl_tm_df.sort_values(start_col, axis=1, ascending=True, inplace=True)
+    zipped_cols = zip(ensembl_tm_df[ensembl_id_col].to_list(),
+                      ensembl_tm_df[start_col].to_list(),
+                      ensembl_tm_df[end_col].to_list())
+
+    ensembl_tm_dict = {}
+    for ensembl_id, start, end in zipped_cols:
+        start, end = int(start), int(end)
+        if ensembl_tm_dict.get(ensembl_id) is None:
+            ensembl_tm_dict[ensembl_id] = [(start, end)]
+        else:
+            ensembl_tm_dict[ensembl_id].append((start, end))
+
+    return ensembl_tm_dict
+
+def apply_ensembl_tm(df, ensembl_tm_dict, ensembl_id_col = "ensembl_peptide_id", seq_col = "sequence",
+                     start_tolerance = 0, end_tolerance = 0):
+    '''
+    Removes Ensembl transmembrane domains from scannable protein sequences
+
+    Args:
+        df (pd.DataFrame):      input dataframe containing sequences to sanitize
+        ensembl_tm_dict (dict): dictionary of ensembl protein id --> list of (start,end) for each transmembrane domain
+        ensembl_id_col (str):   column name in df containing ensembl protein ids
+        seq_col (str):          column name in df containing protein sequences
+        start_tolerance (int):  amount of N-terminal side of transmembrane domain to retain
+        end_tolerance (int):    amount of C-terminal side of transmembrane domain to retain
+
+    Returns:
+        df (pd.DataFrame):      modified input dataframe
+    '''
+
+    for i in np.arange(len(df)):
+        ensembl_peptide_id = df.at[i, ensembl_id_col]
+        tm_ranges = ensembl_tm_dict.get(ensembl_peptide_id)
+        if tm_ranges is not None:
+            protein_seq = df.at[i, seq_col]
+            for start, end in reversed(tm_ranges):
+                start = start + start_tolerance - 1 # convert to 0-indexing
+                end = end + end_tolerance - 1
+                protein_seq = protein_seq[:start] + "X" + protein_seq[end+1:]
+                df.at[i, seq_col] = protein_seq
+
+    return df
