42362f9dff3dcca649e2ab5719866d6f1352d897
noelgarber <104410131+noelgarber@users.noreply.github.com>
2024-07-08 13:32:34 -0400

    Fixed a path error and added forgotten topo_params

--- Motif_Predictor/load_predictor_config.py
+++ Motif_Predictor/load_predictor_config.py
@@ -2,17 +2,19 @@ import os
 import yaml
 import numpy as np
 
-# Load configuration; use local version if present
-cwd = os.getcwd()
-local_path = os.path.join(os.getcwd(), "predictor_params_local.yaml")
-default_path = os.path.join(os.getcwd(), "predictor_params.yaml")
+def load_config(verbose = False):
+    # Load configuration; use local version if present
+    cwd = os.getcwd()
+    if not "Motif_Predictor" in cwd:
+        cwd = os.path.join(cwd.split("PACM")[0], "PACM/Motif_Predictor")
+    local_path = os.path.join(cwd, "predictor_params_local.yaml")
+    default_path = os.path.join(cwd, "predictor_params.yaml")
 
-def load_config(local_path = local_path, default_path = default_path):
     if os.path.exists(local_path):
-        print(f"Found local config file at {local_path}")
+        print(f"Found local config file at {local_path}") if verbose else None
         config_path = local_path
     else:
-        print(f"Using general config file at {default_path}")
+        print(f"Using general config file at {default_path}") if verbose else None
         config_path = default_path
 
     with open(config_path, "r") as config_file:
@@ -24,6 +26,7 @@ def load_config(local_path = local_path, default_path = default_path):
     # Insert other dicts as values in the main dict
     predictor_params["protein_seqs_paths"] = config["protein_seqs_paths"]
     predictor_params["df_chunks"] = config["df_chunks"]
+    predictor_params["topo_params"] = config["topo_params"]
     predictor_params["homology_params"] = config["homology_params"]
     predictor_params["alphafold_params"] = config["alphafold_params"]
 
