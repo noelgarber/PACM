00de0bbecc22f49a2b57786fd74b8c8ff4c61d05
noelgarber <104410131+noelgarber@users.noreply.github.com>
2024-09-05 20:37:23 -0400

    Refactored and added option to save a text report

--- query_gui/query_db.py
+++ query_gui/query_db.py
@@ -188,8 +188,60 @@ def print_entry(gene_id, protein_id, query_dict):
                     for var_key, var_val in value.items():
                         print(f"\t\t\t{var_key}: {var_val}")
 
-def prompt_for_results(motif_len, protein_lengths_dict = None, gene_name_id_dict = {}, gene_id_name_dict = {}):
-    # Create the main window to get the ID
+def entry_to_file(path, gene_protein_ids, query_dict):
+    lines = ["Query Results\n",
+             "\n"]
+
+    multiple_genes = len(gene_protein_ids) > 1
+    for gene_id, protein_ids in gene_protein_ids.items():
+        gene_results = query_dict.get(gene_id)
+        if gene_results is not None:
+            gene_name = gene_results.get("gene_name")
+            lines.append(f"Gene: {gene_name} ({gene_id})\n")
+            lines.append("\n")
+
+            for protein_id in protein_ids:
+                protein_results = gene_results.get(protein_id)
+                if protein_results is not None:
+                    isoform_desc = f"\tIsoform {protein_id}\n"
+                    lines.append(isoform_desc)
+                    lines.append("\n")
+
+                    novel_results = protein_results.get("novel")
+                    if len(novel_results) > 0:
+                        lines.append(f"\t\tNovel model results:\n")
+                        lines.append("\n")
+
+                    for key, value in novel_results.items():
+                        lines.append(f"\t\t\t{key}:\n")
+                        for var_key, var_val in value.items():
+                            lines.append(f"\t\t\t\t{var_key}: {var_val}\n")
+                        lines.append("\n")
+
+                    classical_results = protein_results.get("classical")
+                    classical_results_count = len(classical_results) if classical_results is not None else 0
+                    if classical_results_count > 0:
+                        lines.append(f"\t\tClassical algorithm results:\n")
+                        lines.append("\n")
+
+                        for key, value in classical_results.items():
+                            lines.append(f"\t\t\t{key}:\n")
+                            for var_key, var_val in value.items():
+                                lines.append(f"\t\t\t\t{var_key}: {var_val}\n")
+                        lines.append("\n")
+
+            if multiple_genes:
+                lines.append("-" * 80)
+                lines.append("\n")
+
+    with open(path, "w") as file:
+        file.writelines(lines)
+
+    return lines
+
+def user_prompt():
+    # Initializes a GeneQueryPopup class to collect gene query info from the user
+
     root = tk.Tk()
     root.withdraw()  # Hide the root window
     popup = GeneQueryPopup(root)
@@ -197,71 +249,142 @@ def prompt_for_results(motif_len, protein_lengths_dict = None, gene_name_id_dict
 
     try:
         query_data = popup.query_data.copy()
+
+        gene_name = query_data.get("gene_name")
+        gene_id = query_data.get("gene_id")
+        protein_id = query_data.get("protein_id")
+        query_taxid = query_data.get("query_taxid")
+        homology_taxids = query_data.get("homology_taxids")
+
         root.destroy()
+        valid_input_given = True
+
     except:
         root.destroy()
+        gene_name, gene_id, protein_id, query_taxid, homology_taxids = None, None, None, None, None
         valid_input_given = False
-        return valid_input_given
 
-    # Query the dictionary
-    gene_name = query_data.get("gene_name")
-    gene_id = query_data.get("gene_id")
-    protein_id = query_data.get("protein_id")
-    query_taxid = query_data.get("query_taxid")
-    homology_taxids = query_data.get("homology_taxids")
+    user_inputs = (gene_name, gene_id, protein_id, query_taxid, homology_taxids)
 
-    # Define database to use
-    if correlated_dict is None:
-        query_dict = data_dict.get(int(query_taxid))
-        if query_dict is None:
-            query_dict = data_dict[str(query_taxid)]
-        search_homologs = False
-    else:
-        query_dict = correlated_dict
-        search_homologs = True
+    return user_inputs, valid_input_given
 
-    # Search the database
+def parse_user_input(protein_id, gene_id, gene_name, query_dict, protein_gene_dict, gene_name_id_dict = None,
+                     gene_id_name_dict = None, warn_no_results = True, warn_multiple = True):
+    '''
+    Parses user input depending on what kind of identifier the user provided.
+
+    Args:
+        protein_id (str):         User input from Ensembl protein ID field
+        gene_id (str):            User input from Ensembl gene ID field
+        gene_name (str):          User input from gene name field (must be Ensembl name)
+        query_dict (dict):        Dictionary of data, with or without homology information
+        protein_gene_dict (dict): Dictionary of protein IDs and their corresponding gene IDs
+        gene_name_id_dict (dict): Dictionary of gene names to corresponding gene IDs
+        gene_id_name_dict (dict): Dictionary of gene IDs to corresponding gene names
+        warn_no_results (bool):   Whether to print a warning message if no results were found for the identifier
+        warn_multiple (bool):     Whether to print a warning message if a given gene name matches more than one gene
+
+    Returns:
+        gene_protein_ids (dict):  Dictionary of gene IDs and corresponding lists of protein IDs
+        gene_name (str):          Gene name
+        valid_input_given (bool): Whether the user has provided valid input
+    '''
+
+    valid_input_given = True
+
     if protein_id:
-        valid_input_given = True
         gene_id = protein_gene_dict.get(protein_id)
-        print_entry(gene_id, protein_id, query_dict)
-        if protein_lengths_dict is not None:
-            protein_len = protein_lengths_dict.get(protein_id)
-            motif_domain_map = generate_novel_motif_map(gene_id, protein_id, protein_len, motif_len, query_dict)
+        gene_protein_ids = {gene_id: [protein_id]}
+
     elif gene_id:
-        valid_input_given = True
-        gene_name = gene_id_name_dict.get(gene_id)
+        gene_name = gene_id_name_dict.get(gene_id) if gene_id_name_dict is not None else None
         protein_ids = list(query_dict[gene_id].keys()) if query_dict.get(gene_id) is not None else []
         protein_ids.remove("gene_name")
-
-        motif_domain_maps = {}
-        for protein_id in protein_ids:
-            print_entry(gene_id, protein_id, query_dict)
-            if protein_lengths_dict is not None:
-                protein_len = protein_lengths_dict.get(protein_id)
-                motif_domain_map = generate_novel_motif_map(gene_id, protein_id, protein_len, motif_len, query_dict)
-                motif_domain_maps[protein_id] = motif_domain_map
-
-        merged_map_path = os.path.join(cwd, f"{gene_name}_merged_motif_maps.tif")
-        merged_maps = merge_motif_maps(motif_domain_maps, gene_name)
-        imwrite(merged_map_path, merged_maps)
+        gene_protein_ids = {gene_id: protein_ids}
 
     elif gene_name:
-        valid_input_given = True
+        if gene_name_id_dict is None:
+            raise ValueError(f"gene_name_id_dict was given as None, but must be given when the input is a gene name.")
         gene_ids = gene_name_id_dict.get(gene_name)
         if gene_ids is None:
-            print(f"No results found.")
+            if warn_no_results:
+                print(f"No results found.")
         elif len(gene_ids) > 1:
-            print(f"Caution: \"{gene_name}\" matches multiple Ensembl gene IDs; showing results for each of them.")
+            if warn_multiple:
+                print(f"Caution: \"{gene_name}\" matches multiple Ensembl gene IDs; showing results for each of them.")
 
-        motif_domain_maps = {}
+        gene_protein_ids = {}
         for gene_id in gene_ids:
             print(f"Current gene ID: {gene_id}")
             protein_ids = list(query_dict[gene_id].keys()) if query_dict.get(gene_id) is not None else []
             if "gene_name" in protein_ids:
                 protein_ids.remove("gene_name")
+            gene_protein_ids[gene_id] = protein_ids
+
+    else:
+        gene_protein_ids = {}
+        valid_input_given = False
+
+    return gene_protein_ids, gene_name, valid_input_given
+
+def select_db(data_dict, correlated_dict, query_taxid = None):
+    '''
+    Selects the database to use (with or without homolog information)
+
+    Args:
+        data_dict (dict):            Base dictionary (no homology info)
+        correlated_dict (dict):      Data dictionary containing base info and homology information
+        query_taxid (int):           Base taxonomic identifier given by the user
+
+    Returns:
+        query_dict (dict):      Either data_dict or correlated_dict, depending on which is valid
+        search_homologs (bool): Whether to search homologs; corresponds to whether the correlated_dict was returned
+    '''
+
+    search_homologs = isinstance(correlated_dict, dict)
+    if not search_homologs:
+        query_dict = data_dict.get(int(query_taxid))
+        if query_dict is None:
+            query_dict = data_dict[str(query_taxid)]
+    else:
+        query_dict = correlated_dict
+
+    return query_dict, search_homologs
+
+def prompt_for_results(user_inputs, query_dict, motif_len, search_homologs, protein_lengths_dict = None,
+                       names_to_ids = {}, ids_to_names = {}, print_to_terminal = True):
+    '''
+    Main function for prompting the user and collecting the results.
+
+    Args:
+        user_inputs (tuple):         Tuple of (gene_name, gene_id, protein_id, query_taxid, homology_taxids)
+        query_dict (dict):           Dictionary of data, with or without homology information
+        motif_len (int):             Length of the motif being analyzed
+        search_homologs (bool):      Whether to search for homologous motifs
+        protein_lengths_dict (dict): Dictionary of protein IDs and their corresponding lengths, needed for drawing maps
+        names_to_ids (dict):         Dictionary of gene names to corresponding Ensembl gene IDs
+        ids_to_names (dict):         Dictionary of Ensembl gene IDs to corresponding gene names
+
+    Returns:
+        valid_input_given (bool):    Whether the prompt received valid information that was parsed.
+    '''
+
+    # Create the main window to get the IDs and other information, then parse it
+    gene_name, gene_id, protein_id, query_taxid, homology_taxids = user_inputs
+    gene_protein_ids, gene_name, valid_input_given = parse_user_input(protein_id, gene_id, gene_name, query_dict,
+                                                                      protein_gene_dict, names_to_ids, ids_to_names)
+
+    # Search the database
+    motif_domain_maps = {}
+    if valid_input_given:
+        text_results_path = os.path.join(cwd, f"{gene_name}_motif_results.txt")
+        entry_to_file(text_results_path, gene_protein_ids, query_dict)
+
+        for gene_id, protein_ids in gene_protein_ids.items():
+            print(f"Current gene ID: {gene_id}")
             for protein_id in protein_ids:
-                print_entry(gene_id, protein_id, query_dict)
+                if print_to_terminal:
+                    print_entry(gene_id, protein_id, query_dict)
                 if protein_lengths_dict is not None:
                     protein_len = protein_lengths_dict.get(protein_id)
                     motif_domain_map = generate_novel_motif_map(gene_id, protein_id, protein_len, motif_len, query_dict)
@@ -271,10 +394,7 @@ def prompt_for_results(motif_len, protein_lengths_dict = None, gene_name_id_dict
         merged_maps = merge_motif_maps(motif_domain_maps, gene_name)
         imwrite(merged_map_path, merged_maps)
 
-    else:
-        valid_input_given = False
-
-    return valid_input_given
+    return motif_domain_maps, valid_input_given
 
 if __name__ == "__main__":
     # Load the database
@@ -283,6 +403,12 @@ if __name__ == "__main__":
     protein_lengths, motif_len = get_protein_lengths()
 
     while True:
-        valid_input_given = prompt_for_results(motif_len, protein_lengths, gene_name_id_dict, gene_id_name_dict)
+        user_inputs, valid_input_given = user_prompt()
+        if valid_input_given:
+            query_taxid = user_inputs[3]
+            query_dict, search_homologs = select_db(data_dict, correlated_dict, query_taxid)
+            motif_domain_maps, valid_input_given = prompt_for_results(user_inputs, query_dict, motif_len,
+                                                                      search_homologs, protein_lengths,
+                                                                      gene_name_id_dict, gene_id_name_dict)
         if not valid_input_given:
             break
